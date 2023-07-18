deb91f3a6151abf71479b416d893804bbb161021
thanhvo203 <vohaithanh21@gmail.com>
2023-07-18 16:37:46 +0700

    bai tap

---
+++ module5/ss2/exercise/bai1/src/index.js
@@ -0,0 +1,55 @@
+import React, { createElement } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+const students = [
+  {
+    company: 'Alfreds Futterkiste',
+    contact: 'Maria Anders',
+    country: 'Germany'
+  },
+  {
+    company: 'Centro comercial Moctezuma',
+    contact: 'Francisco Chang',
+    country: 'Mexico'
+  },
+  {
+    company: 'Ernst Handel',
+    contact: 'Roland Mendel',
+    country: 'Austria'
+  },
+  {
+    company: 'Island Trading',
+    contact: 'Helen Bennett',
+    country: 'UK'
+  },
+  {
+    company: 'Laughing Bacchus Winecellars',
+    contact: 'Yoshi Tannamuri',
+    country: 'Canada'
+  },
+  {
+    company: 'Magazzini Alimentari Riuniti',
+    contact: 'Giovanni Rovelli',
+    country: 'Italy'
+  }
+]
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+  <h1>Students</h1>
+  <table>
+       <tr>
+           <th>Company</th>
+           <th>Contact</th>
+           <th>Country</th>
+       </tr>
+       {students.map(student => (
+  <tr>
+    <td>{student.company}</td>
+    <td>{student.company}</td>
+    <td>{student.company}</td>
+  </tr>
+))}
+  </table>
+  </div>
+);
