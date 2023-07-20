6a7be4594e231ca173a9b6f808abfd7ff1cd8355
thanhvo203 <vohaithanh21@gmail.com>
2023-07-20 11:21:19 +0700

    fixed bai tap

--- module5/ss2/exercise/bai1/src/index.js
+++ module5/ss2/exercise/bai1/src/index.js
@@ -3,31 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 const students = [
   {
+    id:1,
     company: 'Alfreds Futterkiste',
     contact: 'Maria Anders',
     country: 'Germany'
   },
   {
+    id:2,
     company: 'Centro comercial Moctezuma',
     contact: 'Francisco Chang',
     country: 'Mexico'
   },
   {
+    id:3,
     company: 'Ernst Handel',
     contact: 'Roland Mendel',
     country: 'Austria'
   },
   {
+    id:4,
     company: 'Island Trading',
     contact: 'Helen Bennett',
     country: 'UK'
   },
   {
+    id:5,
     company: 'Laughing Bacchus Winecellars',
     contact: 'Yoshi Tannamuri',
     country: 'Canada'
   },
   {
+    id:6,
     company: 'Magazzini Alimentari Riuniti',
     contact: 'Giovanni Rovelli',
     country: 'Italy'
@@ -38,18 +44,22 @@ root.render(
   <div>
   <h1>Students</h1>
   <table>
+      <thead>
        <tr>
            <th>Company</th>
            <th>Contact</th>
            <th>Country</th>
        </tr>
+      </thead>
+      <tbody>
        {students.map(student => (
-  <tr>
-    <td>{student.company}</td>
-    <td>{student.company}</td>
-    <td>{student.company}</td>
-  </tr>
-))}
-  </table>
+        <tr key={student.id}>
+            <td>{student.company}</td>
+            <td>{student.contact}</td>
+            <td>{student.country}</td>
+        </tr>
+       ))}
+     </tbody>
+     </table>
   </div>
 );
